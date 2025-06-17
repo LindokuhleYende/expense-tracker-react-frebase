825892071be969fdc0c4d0e96557829e317e0fd8
Lindokuhle <yendelindokuhle2@gmail.com>
2025-06-17 21:18:37 +0200

    Update firebase-config.js

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -26,7 +26,6 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 export const provider = new GoogleAuthProvider()
 export const db = getFirestore(app);
-export const user = auth.currentUser;
 
 //firebase login
 //firebase init
